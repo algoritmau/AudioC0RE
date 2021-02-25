018cc4be69b40c755e8254420d040bc6dc052eb9
Mauricio Paternina <spaceinvadev@gmail.com>
2021-02-25 17:41:38 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  siteMetadata: {
+    title: "AudioC0RE",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "9ymrP1r2PplDUT8-Az4Lvw-r2bj_d5BxDo4brrG6Q8Y",
+        spaceId: "rwz79g8eaa6f",
+      },
+    },
+    "gatsby-plugin-sass",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
