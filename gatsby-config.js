a9e159ba71844b26877f62bc222fd958122c424e
Mauricio Paternina <spaceinvadev@gmail.com>
2021-02-25 18:14:13 -0500

    set up dotenv to hide sensitive tokens

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,30 @@
-module.exports = {
-  siteMetadata: {
-    title: "AudioC0RE",
-  },
-  plugins: [
-    {
-      resolve: "gatsby-source-contentful",
-      options: {
-        accessToken: "9ymrP1r2PplDUT8-Az4Lvw-r2bj_d5BxDo4brrG6Q8Y",
-        spaceId: "rwz79g8eaa6f",
-      },
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+}),
+  (module.exports = {
+    siteMetadata: {
+      title: "AudioC0RE",
     },
-    "gatsby-plugin-sass",
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
+    plugins: [
+      {
+        resolve: "gatsby-source-contentful",
+        options: {
+          accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+          spaceId: process.env.CONTENTFUL_SPACE_ID,
+        },
       },
-      __key: "images",
-    },
-  ],
-};
+      "gatsby-plugin-sass",
+      "gatsby-plugin-sharp",
+      "gatsby-plugin-react-helmet",
+      "gatsby-plugin-sitemap",
+      "gatsby-transformer-sharp",
+      {
+        resolve: "gatsby-source-filesystem",
+        options: {
+          name: "images",
+          path: "./src/images/",
+        },
+        __key: "images",
+      },
+    ],
+  });
